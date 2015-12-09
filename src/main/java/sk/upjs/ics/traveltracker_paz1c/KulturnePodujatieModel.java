4a7987b47d9c2c46c4654c95cfd2da0cdd7d4c68
Pandita <Pandita@Oracle>
2015-12-09 15:48:12 +0100

    vytvorenie triedy KulturnePodujatieModel pre KulturnePodujatieForm (nekodoncene)

--- src/main/java/sk/upjs/ics/traveltracker_paz1c/KulturnePodujatieModel.java
+++ src/main/java/sk/upjs/ics/traveltracker_paz1c/KulturnePodujatieModel.java
@@ -4,16 +4,29 @@ package sk.upjs.ics.traveltracker_paz1c;
 import javax.swing.table.AbstractTableModel;
 
 public class KulturnePodujatieModel extends AbstractTableModel {
+    
+    private MySqlKulturnePodujatieDao podujatieDao = new MySqlKulturnePodujatieDao();
+    private static final int POCET_RIADKOV = 10;
+    private static final int POCET_STLPCOV = 4;
+    private static final String[] NAZVY_STLPCOV = {"Krajina", "Mesto", "Názov", "Dátum"};
+    
 
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return POCET_RIADKOV;
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return POCET_STLPCOV;
     }
+
+    @Override
+    public String getColumnName(int column) {
+        return NAZVY_STLPCOV[column];
+    }
+    
+    
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
